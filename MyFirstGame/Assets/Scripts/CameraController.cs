89caa64c2e00870e586848d22e6adca055bb90c2
almslsbest <185805234@qq.com>
2024-07-15 14:30:03 +0800

    first game 素材雏形

---
+++ MyFirstGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float zoomSpeed = 5f;
+
+    private Vector3 offset;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + offset;
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        // Debug.Log(f);
+
+        Camera.main.fieldOfView += scroll * zoomSpeed;
+
+        Camera.main.fieldOfView = Math.Clamp(Camera.main.fieldOfView,35,68);
+    }
+}
